2defc6eb42662836787c50cb1477495196b62246
wangqiubao <wangqiubao@u-care.net>
2019-02-15 15:49:10 +0800

    做了几个测试

--- src/main/java/com/criss/wang/controller/RedisController.java
+++ src/main/java/com/criss/wang/controller/RedisController.java
@@ -1,5 +1,7 @@
 package com.criss.wang.controller;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,10 +24,22 @@ public class RedisController {
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String testSet() {
+	public String testSet() throws InterruptedException {
 		String vo = "kdjfkd";
 		this.localRedisTemplate.opsForValue().set("study", vo);
 		this.defaultRedisTemplate.opsForValue().set("aa", vo);
+
+
+		localRedisTemplate.opsForValue().set("key", "value", 7000, TimeUnit.MILLISECONDS);
+		String ans = localRedisTemplate.opsForValue().get("key");
+		System.out.println("value".equals(ans));
+		TimeUnit.MILLISECONDS.sleep(6990);
+		ans = localRedisTemplate.opsForValue().get("key");
+		System.out.println("value".equals(ans) + " >> false ans should be null! ans=[" + ans + "]");
+
+		defaultRedisTemplate.opsForValue().set("key", "value", 10000, TimeUnit.MILLISECONDS);
+		ans = defaultRedisTemplate.opsForValue().get("key");
+		System.out.println(ans);
 		return "success";
 	}
 }
