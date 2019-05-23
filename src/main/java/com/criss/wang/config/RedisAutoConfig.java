be514a120d6a8038bc30c4c842450492ebda1256
wangqiubao <wangqiubao@u-care.net>
2019-05-23 16:10:15 +0800

    三个数据源的配置

--- src/main/java/com/criss/wang/config/RedisAutoConfig.java
+++ src/main/java/com/criss/wang/config/RedisAutoConfig.java
@@ -25,146 +25,213 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  *
- * author: wangqiubao
+ * @author wangqiubao
  *
- * date: 2019-02-15 15:49:46
+ * @date 2019-05-23 15:47
  *
- * describe: 配置多个redis数据源
+ * @description redis多数据源配置
  */
 @Configuration
 public class RedisAutoConfig {
-
-	// 注意：有多个ConnectionFactory存在时，在创建默认的RedisTemplate就不知道该选择哪一个了，
-	// 所以需要借助@Primary来指定默认的连接工厂，然后在使用工程的时候，通过@Qualifier注解来显示指定，
-	// 我需要的工厂是哪个（主要是localRedisTemplate这个bean的定义，如果不加，则会根据defaultLettuceConnectionFactory这个实例来创建Redis连接了）
-
-	@Primary
-	@Bean
-	public LettuceConnectionFactory defaultLettuceConnectionFactory(RedisStandaloneConfiguration defaultRedisConfig,
-			GenericObjectPoolConfig defaultPoolConfig) {
-		LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
-				.commandTimeout(Duration.ofMillis(100)).poolConfig(defaultPoolConfig).build();
-		return new LettuceConnectionFactory(defaultRedisConfig, clientConfig);
-	}
-
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Bean
-	public RedisTemplate<String, String> defaultRedisTemplate( @Qualifier("defaultLettuceConnectionFactory") LettuceConnectionFactory defaultLettuceConnectionFactory) {
-		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(defaultLettuceConnectionFactory);
-		redisTemplate.setKeySerializer(new StringRedisSerializer()); // key的序列化类型
-		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-		ObjectMapper om = new ObjectMapper();
-		om.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
-		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer); // value序列化为json
-		return redisTemplate;
-	}
-
-	@Bean
-	@ConditionalOnBean(name = "localRedisConfig")
-	public LettuceConnectionFactory localLettuceConnectionFactory(RedisStandaloneConfiguration localRedisConfig,
-			GenericObjectPoolConfig localPoolConfig) {
-		LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
-				.commandTimeout(Duration.ofMillis(100)).poolConfig(localPoolConfig).build();
-		return new LettuceConnectionFactory(localRedisConfig, clientConfig);
-	}
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Bean
-	@ConditionalOnBean(name = "localLettuceConnectionFactory")
-	public RedisTemplate<String, String> localRedisTemplate(@Qualifier("localLettuceConnectionFactory") LettuceConnectionFactory localLettuceConnectionFactory) {
-		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(localLettuceConnectionFactory);
-		redisTemplate.setKeySerializer(new StringRedisSerializer()); // key的序列化类型
-		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-		ObjectMapper om = new ObjectMapper();
-		om.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
-		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer); // value序列化为json
-		return redisTemplate;
-	}
-
-	@Configuration
-	@ConditionalOnProperty(name = "host", prefix = "spring.local-redis")
-	public static class LocalRedisConfig {
-		@Value("${spring.local-redis.host}")
-		private String host;
-		@Value("${spring.local-redis.port}")
-		private Integer port;
-		@Value("${spring.local-redis.password}")
-		private String password;
-		@Value("${spring.local-redis.database}")
-		private Integer database;
-
-		@Value("${spring.local-redis.lettuce.pool.max-active}")
-		private Integer maxActive;
-		@Value("${spring.local-redis.lettuce.pool.max-idle}")
-		private Integer maxIdle;
-		@Value("${spring.local-redis.lettuce.pool.max-wait}")
-		private Long maxWait;
-		@Value("${spring.local-redis.lettuce.pool.min-idle}")
-		private Integer minIdle;
-
+	//---------------------redis-default-------------------
+		@Primary
 		@Bean
-		public GenericObjectPoolConfig localPoolConfig() {
-			GenericObjectPoolConfig config = new GenericObjectPoolConfig();
-			config.setMaxTotal(maxActive);
-			config.setMaxIdle(maxIdle);
-			config.setMinIdle(minIdle);
-			config.setMaxWaitMillis(maxWait);
-			return config;
+		public LettuceConnectionFactory defaultLettuceConnectionFactory(RedisStandaloneConfiguration defaultRedisConfig,
+				GenericObjectPoolConfig defaultPoolConfig) {
+			LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+					.commandTimeout(Duration.ofMillis(100)).poolConfig(defaultPoolConfig).build();
+			return new LettuceConnectionFactory(defaultRedisConfig, clientConfig);
 		}
 
-		@Bean
-		public RedisStandaloneConfiguration localRedisConfig() {
-			RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-			config.setHostName(host);
-			config.setPassword(RedisPassword.of(password));
-			config.setPort(port);
-			config.setDatabase(database);
-			return config;
-		}
-	}
-
-	@Configuration
-	public static class DefaultRedisConfig {
-		@Value("${spring.redis.host}")
-		private String host;
-		@Value("${spring.redis.port}")
-		private Integer port;
-		@Value("${spring.redis.password}")
-		private String password;
-		@Value("${spring.redis.database}")
-		private Integer database;
-
-		@Value("${spring.redis.lettuce.pool.max-active}")
-		private Integer maxActive;
-		@Value("${spring.redis.lettuce.pool.max-idle}")
-		private Integer maxIdle;
-		@Value("${spring.redis.lettuce.pool.max-wait}")
-		private Long maxWait;
-		@Value("${spring.redis.lettuce.pool.min-idle}")
-		private Integer minIdle;
-
-		@Bean
-		public GenericObjectPoolConfig defaultPoolConfig() {
-			GenericObjectPoolConfig config = new GenericObjectPoolConfig();
-			config.setMaxTotal(maxActive);
-			config.setMaxIdle(maxIdle);
-			config.setMinIdle(minIdle);
-			config.setMaxWaitMillis(maxWait);
-			return config;
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		@Bean("redisTemplateDB1")
+		public RedisTemplate<String, String> defaultRedisTemplate( @Qualifier("defaultLettuceConnectionFactory") LettuceConnectionFactory defaultLettuceConnectionFactory) {
+			RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+			redisTemplate.setConnectionFactory(defaultLettuceConnectionFactory);
+			redisTemplate.setKeySerializer(new StringRedisSerializer()); // key的序列化类型
+			Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+			ObjectMapper om = new ObjectMapper();
+			om.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
+			om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+			redisTemplate.setValueSerializer(jackson2JsonRedisSerializer); // value序列化为json
+			return redisTemplate;
 		}
 
+		//--------------------redis-local------------------
+
 		@Bean
-		public RedisStandaloneConfiguration defaultRedisConfig() {
-			RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-			config.setHostName(host);
-			config.setPassword(RedisPassword.of(password));
-			config.setPort(port);
-			config.setDatabase(database);
-			return config;
+		@ConditionalOnBean(name = "twoRedisConfig")
+		public LettuceConnectionFactory twoLettuceConnectionFactory(RedisStandaloneConfiguration twoRedisConfig,
+				GenericObjectPoolConfig twoPoolConfig) {
+			LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+					.commandTimeout(Duration.ofMillis(100)).poolConfig(twoPoolConfig).build();
+			return new LettuceConnectionFactory(twoRedisConfig, clientConfig);
 		}
-	}
+
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		@Bean("redisTemplateDB2")
+		@ConditionalOnBean(name = "twoLettuceConnectionFactory")
+		public RedisTemplate<String, String> twoRedisTemplate(@Qualifier("twoLettuceConnectionFactory") LettuceConnectionFactory twoLettuceConnectionFactory) {
+			RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+			redisTemplate.setConnectionFactory(twoLettuceConnectionFactory);
+			redisTemplate.setKeySerializer(new StringRedisSerializer()); // key的序列化类型
+			Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+			ObjectMapper om = new ObjectMapper();
+			om.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
+			om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+			redisTemplate.setValueSerializer(jackson2JsonRedisSerializer); // value序列化为json
+			return redisTemplate;
+		}
+
+		//---------------redis-three--------------
+
+		@Bean
+		@ConditionalOnBean(name = "threeRedisConfig")
+		public LettuceConnectionFactory threeLettuceConnectionFactory(RedisStandaloneConfiguration threeRedisConfig,
+				GenericObjectPoolConfig threePoolConfig) {
+			LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+					.commandTimeout(Duration.ofMillis(100)).poolConfig(threePoolConfig).build();
+			return new LettuceConnectionFactory(threeRedisConfig, clientConfig);
+		}
+
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		@Bean("redisTemplateDB3")
+		@ConditionalOnBean(name = "threeLettuceConnectionFactory")
+		public RedisTemplate<String, String> threeRedisTemplate(@Qualifier("threeLettuceConnectionFactory") LettuceConnectionFactory threeLettuceConnectionFactory) {
+			RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+			redisTemplate.setConnectionFactory(threeLettuceConnectionFactory);
+			redisTemplate.setKeySerializer(new StringRedisSerializer()); // key的序列化类型
+			Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+			ObjectMapper om = new ObjectMapper();
+			om.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
+			om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+			redisTemplate.setValueSerializer(jackson2JsonRedisSerializer); // value序列化为json
+			return redisTemplate;
+		}
+
+		@Configuration
+		@ConditionalOnProperty(name = "host", prefix = "spring.redis-two")
+		public static class TwoRedisConfig {
+			@Value("${spring.redis-two.host}")
+			private String host;
+			@Value("${spring.redis-two.port}")
+			private Integer port;
+			@Value("${spring.redis-two.password}")
+			private String password;
+			@Value("${spring.redis-two.database}")
+			private Integer database;
+
+			@Value("${spring.redis-two.lettuce.pool.max-active}")
+			private Integer maxActive;
+			@Value("${spring.redis-two.lettuce.pool.max-idle}")
+			private Integer maxIdle;
+			@Value("${spring.redis-two.lettuce.pool.max-wait}")
+			private Long maxWait;
+			@Value("${spring.redis-two.lettuce.pool.min-idle}")
+			private Integer minIdle;
+
+			@Bean
+			public GenericObjectPoolConfig twoPoolConfig() {
+				GenericObjectPoolConfig config = new GenericObjectPoolConfig();
+				config.setMaxTotal(maxActive);
+				config.setMaxIdle(maxIdle);
+				config.setMinIdle(minIdle);
+				config.setMaxWaitMillis(maxWait);
+				return config;
+			}
+
+			@Bean
+			public RedisStandaloneConfiguration twoRedisConfig() {
+				RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+				config.setHostName(host);
+				config.setPassword(RedisPassword.of(password));
+				config.setPort(port);
+				config.setDatabase(database);
+				return config;
+			}
+		}
+
+
+		@Configuration
+		@ConditionalOnProperty(name = "host", prefix = "spring.redis-three")
+		public static class RedisThreeConfig {
+			@Value("${spring.redis-three.host}")
+			private String host;
+			@Value("${spring.redis-three.port}")
+			private Integer port;
+			@Value("${spring.redis-three.password}")
+			private String password;
+			@Value("${spring.redis-three.database}")
+			private Integer database;
+
+			@Value("${spring.redis-three.lettuce.pool.max-active}")
+			private Integer maxActive;
+			@Value("${spring.redis-three.lettuce.pool.max-idle}")
+			private Integer maxIdle;
+			@Value("${spring.redis-three.lettuce.pool.max-wait}")
+			private Long maxWait;
+			@Value("${spring.redis-three.lettuce.pool.min-idle}")
+			private Integer minIdle;
+
+			@Bean
+			public GenericObjectPoolConfig threePoolConfig() {
+				GenericObjectPoolConfig config = new GenericObjectPoolConfig();
+				config.setMaxTotal(maxActive);
+				config.setMaxIdle(maxIdle);
+				config.setMinIdle(minIdle);
+				config.setMaxWaitMillis(maxWait);
+				return config;
+			}
+
+			@Bean
+			public RedisStandaloneConfiguration threeRedisConfig() {
+				RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+				config.setHostName(host);
+				config.setPassword(RedisPassword.of(password));
+				config.setPort(port);
+				config.setDatabase(database);
+				return config;
+			}
+		}
+
+		@Configuration
+		public static class DefaultRedisConfig {
+			@Value("${spring.redis.host}")
+			private String host;
+			@Value("${spring.redis.port}")
+			private Integer port;
+			@Value("${spring.redis.password}")
+			private String password;
+			@Value("${spring.redis.database}")
+			private Integer database;
+
+			@Value("${spring.redis.lettuce.pool.max-active}")
+			private Integer maxActive;
+			@Value("${spring.redis.lettuce.pool.max-idle}")
+			private Integer maxIdle;
+			@Value("${spring.redis.lettuce.pool.max-wait}")
+			private Long maxWait;
+			@Value("${spring.redis.lettuce.pool.min-idle}")
+			private Integer minIdle;
+
+			@Bean
+			public GenericObjectPoolConfig defaultPoolConfig() {
+				GenericObjectPoolConfig config = new GenericObjectPoolConfig();
+				config.setMaxTotal(maxActive);
+				config.setMaxIdle(maxIdle);
+				config.setMinIdle(minIdle);
+				config.setMaxWaitMillis(maxWait);
+				return config;
+			}
+
+			@Bean
+			public RedisStandaloneConfiguration defaultRedisConfig() {
+				RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+				config.setHostName(host);
+				config.setPassword(RedisPassword.of(password));
+				config.setPort(port);
+				config.setDatabase(database);
+				return config;
+			}
+		}
 }
--- src/main/java/com/criss/wang/controller/RedisController.java
+++ src/main/java/com/criss/wang/controller/RedisController.java
@@ -20,33 +20,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisController {
 
 	@Autowired
-	private RedisTemplate<String, String> localRedisTemplate;
+	private RedisTemplate<String, String> redisTemplateDB2;
 
 	@Autowired
-	private RedisTemplate<String, String> defaultRedisTemplate;
+	private RedisTemplate<String, String> redisTemplateDB1;
+
+	@Autowired
+	private RedisTemplate<String, String> redisTemplateDB3;
 
 	@RequestMapping(value = "/get/value", method = RequestMethod.GET)
 	public String testGet() {
-		System.out.println(this.localRedisTemplate.opsForValue().get("study"));
+		System.out.println(this.redisTemplateDB2.opsForValue().get("study"));
 		return "success";
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String testSet() throws InterruptedException {
 		String vo = "kdjfkd";
-		this.localRedisTemplate.opsForValue().set("db", vo);
-		this.defaultRedisTemplate.opsForValue().set("db", vo);
+		this.redisTemplateDB2.opsForValue().set("db", vo);
+		this.redisTemplateDB1.opsForValue().set("db", vo);
+		this.redisTemplateDB3.opsForValue().set("db", vo);
 
 
-		localRedisTemplate.opsForValue().set("key", "value", 7000, TimeUnit.MILLISECONDS);
-		String ans = localRedisTemplate.opsForValue().get("key");
+		redisTemplateDB2.opsForValue().set("key", "value", 7000, TimeUnit.MILLISECONDS);
+		String ans = redisTemplateDB2.opsForValue().get("key");
 		System.out.println("value".equals(ans));
 		TimeUnit.MILLISECONDS.sleep(6990);
-		ans = localRedisTemplate.opsForValue().get("key");
+		ans = redisTemplateDB2.opsForValue().get("key");
 		System.out.println("value".equals(ans) + " >> false ans should be null! ans=[" + ans + "]");
 
-		defaultRedisTemplate.opsForValue().set("key", "value", 10000, TimeUnit.MILLISECONDS);
-		ans = defaultRedisTemplate.opsForValue().get("key");
+		redisTemplateDB1.opsForValue().set("key", "value", 10000, TimeUnit.MILLISECONDS);
+		ans = redisTemplateDB1.opsForValue().get("key");
 		System.out.println(ans);
 		return "success";
 	}
